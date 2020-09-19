80cbf43cb428345125e7743898d4eeb7bf2482e9
Wyjbuss <wyjbuss@gmail.com>
2020-09-19 13:12:10 -0500

    port updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // used/required in order to start using body parser in our code
 // parses the html file
